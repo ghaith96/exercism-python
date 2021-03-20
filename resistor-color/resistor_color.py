0d725b5c8b2b7ac306e7b7855c8cb37dacedc5df
ghaith jardaneh <ghaithjardane@gmail.com>
2021-03-20 13:19:16 +0000

    solve resistory_color

---
+++ resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+colors_arr = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+]
+
+
+def color_code(color):
+    return colors_arr.index(color)
+
+
+def colors():
+    return colors_arr
