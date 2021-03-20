d5c9a4a27698a50e1a393f1a7cf2448648661e0c
ghaith jardaneh <ghaithjardane@gmail.com>
2021-03-20 08:28:27 +0000

    improve on raindrop solution

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,11 +1,6 @@
 def convert(number):
     result = ""
-    if number % 3 == 0:
-        result += "Pling"
-    if number % 5 == 0:
-        result += "Plang"
-    if number % 7 == 0:
-        result += "Plong"
-    if result == "":
-        result = f"{number}"
-    return result
+    result += "Pling" if number % 3 == 0 else ""
+    result += "Plang" if number % 5 == 0 else ""
+    result += "Plong" if number % 7 == 0 else ""
+    return result if result else f"{number}"
