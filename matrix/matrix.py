36e5c89e488e93f2f3435a1501e1735c3179f0e7
ghaith jardaneh <ghaithjardane@gmail.com>
2021-04-01 16:47:05 +0000

    improve on matrix solution

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,14 +1,14 @@
 class Matrix:
     def __init__(self, matrix_string: str):
         # another solution (functional)
-        # [list(map(int, elements.split(" "))) for elements in matrix_string.split("\n")]
+        # [list(map(int, elements.split())) for elements in matrix_string.splitlines()]
         self.matrix = [
-            [int(element) for element in elements.split(" ")]
-            for elements in matrix_string.split("\n")
+            [int(element) for element in elements.split()]
+            for elements in matrix_string.splitlines()
         ]
 
     def row(self, index):
-        return self.matrix[index - 1]
+        return self.matrix[index - 1].copy()
 
     def column(self, index):
         return [row[index - 1] for row in self.matrix]
